65eb727a2a84297ff29d82a559c29241ca981817
Dan Knorr <Daniel.Knorr@gmail.com>
2022-08-31 20:26:06 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+   name='rowing',
+   version='1.0',
+   description='Rowing related functions.',
+   author='Dan Knorr',
+   packages=['rowing'],  # would be the same as name
+#    install_requires=['wheel', 'bar', 'greek'], #external packages acting as dependencies
+)
