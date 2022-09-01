e2bd3489783670b0adaac07684139e6038cb7c6f
Dan Knorr <Daniel.Knorr@gmail.com>
2022-08-31 20:27:17 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ setup(
    version='1.0',
    description='Rowing related functions.',
    author='Dan Knorr',
-   packages=['rowing'],  # would be the same as name
+#    packages=['rowing'],  # would be the same as name
 #    install_requires=['wheel', 'bar', 'greek'], #external packages acting as dependencies
 )
